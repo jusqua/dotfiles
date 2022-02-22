40cc125973b2b1c47621a9e903212f774ecc3f56
jusqua <adrian.gama.se@gmail.com>
2022-02-22 18:14:28 -0300

    Add options.lua

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+print("W.I.P.")
+
+require "options"
+
---
+++ nvim/lua/options.lua
@@ -0,0 +1,39 @@
+-- Aliases
+local set = vim.opt
+local cmd = vim.cmd
+
+-- Line indicator
+set.number = true
+set.relativenumber = true
+set.numberwidth = 4
+set.cursorline = true
+set.scrolloff = 8
+set.sidescrolloff = 8
+
+-- Tabulation
+set.expendtab = true
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
+
+-- File manipulation
+set.fileencoding = "utf-8"
+set.backup = false
+set.swapfile = false
+set.undofile = true
+set.clipboard = "unnamedplus"
+
+-- Visualization
+set.wrap = false
+set.splitbelow = true
+set.splitright = true
+set.mouse = "a"
+set.termguicolors = true
+set.conceallevel = 0
+
+-- Search
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
