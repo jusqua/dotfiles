89f7a9ad05899efb8ed2bd11ec5818162ce96831
jusqua <adrian.gama.se@gmail.com>
2022-03-20 08:11:41 -0300

    Update autocommands.lua

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,12 +1,23 @@
 -- Aliases
 local cmd = function (cmd) vim.api.nvim_exec(cmd, false) end
 
--- Autocommands
+-- Terminam mode config
 cmd [[
   augroup terminal
     autocmd!
     autocmd TermOpen * startinsert
     autocmd TermOpen * :set nonumber norelativenumber
-  augroup END 
+  augroup end
+]]
+
+cmd [[
+  augroup lastposition
+    autocmd!
+    autocmd BufReadPost *
+      \ if &ft != "gitcommit" && &ft != "gitrebase" &&
+      \ line("'\"") > 1 && line("'\"") <= line("$") |
+      \ exe "normal! g`\"" |
+      \ endif
+  augroup end
 ]]
 
