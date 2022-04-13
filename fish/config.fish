79620f797bffc8ed6cb789342e839cad04fd12a7
Ádrian Gama <adrian.gama.se@gmail.com>
2022-04-12 23:45:20 -0300

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,51 @@
-# Path setup
+# Prevent user path overflow
 set -e fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+
+# Add local binaries to path
+set -p fish_user_paths $HOME/.local/bin
 
 # Supresses intro message
 set fish_greeting
 
 # Set terminal type
-set TERM "xterm-256color"
+set TERM alacritty
 
 # Set default terminal editor
-set EDITOR "nvim"                
+set EDITOR nvim
 
-# Set default manpager
-# set -x MANPAGER "nvim -c 'set ft=man' -"
+# Use Vi mode
+set fish_user_key_bindings fish_vi_key_bindings
+
+# Aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+alias e=$EDITOR
+
+alias ls='exa --color=always --group-directories-first' 
+alias la='exa -a --color=always --group-directories-first' 
+alias ll='exa -la --color=always --group-directories-first' 
+alias lt='exa -aT --color=always --group-directories-first' 
+
+alias cat='bat'
+
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+alias add='git add'
+alias addall='git add . && git status --short ;;'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull'
+alias push='git push'
+alias info='git status --short'
+
+# Start Starship
+starship init fish | source
 
