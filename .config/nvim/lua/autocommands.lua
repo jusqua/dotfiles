308167e80301a4c10ce55b951ca57fc7646d74b9
Ádrian Gama <adrian.gama.se@gmail.com>
2022-04-23 16:49:31 -0300

    nvim: update autocommands to pure lua

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,29 +1,56 @@
 -- : Aliases
-local function cmd (string)
-  vim.api.nvim_exec(string, false)
-end
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
 
--- : Terminal mode startup config
--- Starts in insert mode
--- Starts without numbers
-cmd [[
-  augroup terminal
-    autocmd!
-    autocmd TermOpen * startinsert
-    autocmd TermOpen * :set nonumber norelativenumber
-  augroup end
-]]
+-- : Tweaks
+-- Highlight yanks
+augroup("YankHighlight", { clear = true })
+autocmd("TextYankPost", {
+  group = "YankHighlight",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = "1000" })
+  end
+})
+-- Remove whitespace when write
+autocmd("BufWritePre", {
+  pattern = "*",
+  command = ":%s/\\s\\+$//e"
+})
+-- Don't auto commenting new lines
+autocmd("BufEnter", {
+  pattern = "*",
+  command = "set fo-=c fo-=r fo-=o"
+})
 
--- : Opening file start on the last line
--- Ignores git files for this
-cmd [[
-  augroup lastposition
-    autocmd!
-    autocmd BufReadPost *
-      \ if &ft != "gitcommit" && &ft != "gitrebase" &&
-      \ line("'\"") > 1 && line("'\"") <= line("$") |
-      \ exe "normal! g`\"" |
-      \ endif
-  augroup end
-]]
+-- : Terminal
+-- Enter insert mode when switching to terminal
+autocmd("TermOpen", {
+  command = "setlocal nonumber norelativenumber nocursorline noshowmode noshowcmd"
+})
+-- Changes the terminal tab to terminal
+autocmd("TermOpen", {
+  command = "file Terminal"
+})
+-- Start in insert mode
+autocmd("TermOpen", {
+  pattern = "*",
+  command = "startinsert"
+})
+-- Close terminal buffer on process exit
+autocmd("BufLeave", {
+  pattern = "term://*",
+  command = "stopinsert"
+})
+
+-- : Opening file start on the last line, and ignores git files
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line
+    local filetype = vim.bo.filetype
+    if filetype ~= "gitcommit" and filetype ~= "gitrebase" and line("'\"") > 0 and line("'\"") <= line("$") then
+      vim.cmd("normal! g`\"zvzz'")
+    end
+  end
+})
 
