70cdcd065795327e3af08f4ebe6eca94649eb329
Ádrian Gama <adrian.gama.se@gmail.com>
2022-04-23 23:55:10 -0300

    nvim: setup statusline

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -1,5 +1,21 @@
 -- : Checking package
 local ok, lualine = pcall(require, 'lualine')
-if not _ok then
+if not ok then
   return
 end
+
+-- : Setup
+lualine.setup {
+  options = {
+    globalstatus = true
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'filename' },
+    lualine_c = { 'branch' },
+    lualine_x = { 'diagnostics' },
+    lualine_y = { 'filetype' },
+    lualine_z = { 'progress' }
+  },
+}
+
