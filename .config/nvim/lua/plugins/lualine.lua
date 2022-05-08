672deca09a11a3fd73a37954c28a611f33e2e33e
Ádrian Gama <adrian.gama.se@gmail.com>
2022-05-08 17:17:26 -0300

    nvim: change lualine apperance

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,17 +8,17 @@ end
 lualine.setup {
   options = {
     globalstatus = true,
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = { left = '·', right = '·' },
+    section_separators = { left = '|', right = '|' },
     disabled_filetypes = { "toggleterm" }
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
-    lualine_c = {},
-    lualine_x = { 'diagnostics' },
-    lualine_y = { 'filetype' },
-    lualine_z = { 'progress' }
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'filetype', 'diagnostics' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
 }
 
