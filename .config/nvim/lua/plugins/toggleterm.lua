8fff447eecb07f48d7366c95fd35ffda60f013f8
Ádrian Gama <adrian.gama.se@gmail.com>
2022-05-08 17:09:44 -0300

    nvim: change toggleterm apperance

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -14,15 +14,10 @@ toggleterm.setup({
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
+  close_on_exit = true,
 	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
 	float_opts = {
-		border = "curved",
+		border = "double",
 		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
 	},
 })
