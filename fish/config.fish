ada6bb1898e549685144dafdb379f0af872c5608
Ádrian Gama <adrian.gama.se@gmail.com>
2024-10-16 10:47:35 -0300

    add custom `fish_prompt` to "fish" config files

--- fish/config.fish
+++ fish/config.fish
@@ -2,32 +2,40 @@ function fish_greeting
     # Display nothing at all
 end
 
-fish_add_path $HOME/.local/bin
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+    set -q fish_color_status
+    or set -g fish_color_status red
 
-# Catppuccin Mocha Theme
-set -g fish_color_normal cdd6f4
-set -g fish_color_command 89b4fa
-set -g fish_color_param f2cdcd
-set -g fish_color_keyword f38ba8
-set -g fish_color_quote a6e3a1
-set -g fish_color_redirection f5c2e7
-set -g fish_color_end fab387
-set -g fish_color_comment 7f849c
-set -g fish_color_error f38ba8
-set -g fish_color_gray 6c7086
-set -g fish_color_selection --background=313244
-set -g fish_color_search_match --background=313244
-set -g fish_color_option a6e3a1
-set -g fish_color_operator f5c2e7
-set -g fish_color_escape eba0ac
-set -g fish_color_autosuggestion 6c7086
-set -g fish_color_cancel f38ba8
-set -g fish_color_cwd f9e2af
-set -g fish_color_user 94e2d5
-set -g fish_color_host 89b4fa
-set -g fish_color_host_remote a6e3a1
-set -g fish_color_status f38ba8
-set -g fish_pager_color_progress 6c7086
-set -g fish_pager_color_prefix f5c2e7
-set -g fish_pager_color_completion cdd6f4
-set -g fish_pager_color_description 6c7086
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l suffix '>'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix '#'
+    end
+
+    # Write pipestatus
+    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
+    end
+    set __fish_prompt_status_generation $status_generation
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color $bold_flag $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+    # Verify if is inside a container
+    set -l _container_id ""
+    if test $CONTAINER_ID
+        set _container_id "[$CONTAINER_ID] "
+    end
+
+    echo -n -s $_container_id (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+end
